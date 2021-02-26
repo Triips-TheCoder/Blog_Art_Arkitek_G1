22eadb24567b61323c661f2428f56f61849c3063
TheoSaminadin <theo.saminadin@mmibordeaux.com>
2021-02-26 16:17:56 +0100

    correction casse

--- BLOGART21/front/includes/Inscription.php
+++ BLOGART21/front/includes/Inscription.php
@@ -3,21 +3,6 @@ require_once __DIR__ . '/../../util/utilErrOn.php';
 require_once __DIR__ . '../../../CLASS_CRUD/membre.class.php';
 require_once __DIR__ . '/../../util/ctrlSaisies.php';
 
-function ctrlSaisies($saisie){
-
-    // Convertion caractères spéciaux en entités HTML => peu performant
-    // Préférer htmlentities()
-    $saisie = htmlspecialchars($saisie);
-    // Suppression des espaces (ou d'autres caractères) en début et fin de chaîne
-    $saisie = trim($saisie);
-    // Suppression des antislashs d'une chaîne
-    $saisie = stripslashes($saisie);
-    // Conversion des caractères spéciaux en entités HTML
-    $saisie = htmlentities($saisie);
-
-    return $saisie;
-  }
-
 $monMembre = new membre;
 
 $passOk = 0; 
