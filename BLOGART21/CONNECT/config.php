199abe9d6eb5131e8bdc2700d0f79f3f4c6ad710
TheoSaminadin <theo.saminadin@mmibordeaux.com>
2021-02-26 14:00:41 +0100

    ajout id

--- BLOGART21/CONNECT/config.php
+++ BLOGART21/CONNECT/config.php
@@ -1,14 +1,28 @@
 <?
 
-  // nom de votre serveur (ou 127.0.0.1)
-  $hostBD = "localhost";
-  // nom BD
-  $nomBD = "blogart21";
-  // Serveur
-  // Avec encodage UTF8
-  $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
+  // // nom de votre serveur (ou 127.0.0.1)
+  // $hostBD = "localhost";
+  // // nom BD
+  // $nomBD = "blogart21";
+  // // Serveur
+  // // Avec encodage UTF8
+  // $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
 
-  // nom utilisateur de connexion à la BDD
-  $userBD = 'root';         // Votre login
-  // mot de passe de connexion à la BDD
-  $passBD = '';         // Votre Pass
+  // // nom utilisateur de connexion à la BDD
+  // $userBD = 'root';         // Votre login
+  // // mot de passe de connexion à la BDD
+  // $passBD = '';         // Votre Pass
+
+    // nom de votre serveur (ou 127.0.0.1)
+    $hostBD = "localhost";
+    // nom BD
+    $nomBD = "wp_user01";
+    // Serveur
+    // Avec encodage UTF8
+    $serverBD = "mysql:dbname=$nomBD;host=$hostBD;charset=utf8";
+  
+    // nom utilisateur de connexion à la BDD
+    $userBD = 'user_db_01';         // Votre login
+    // mot de passe de connexion à la BDD
+    $passBD = 'mmi-2020';         // Votre Pass
+  
